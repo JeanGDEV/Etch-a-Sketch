a572959dfedd16654b7bbb70fa4281f4eb7850f7
JeanGDEV <123316415+JeanGDEV@users.noreply.github.com>
2026-08-24 17:17:23 -0400

    Fix grid doesn't remove when create one

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const size = containerWidth / 16
 
 initializeGrid()
 
+resetBtn.addEventListener("click", () => newGrid())
 
 function initializeGrid() {
     for (let i = 0; i < 16; i++) {
@@ -20,6 +21,25 @@ function initializeGrid() {
             square.style.border = "1px solid black"
             square.addEventListener("mouseover", (event) => {
                 event.target.style.backgroundColor = "tomato"
+            })
+        }
+    }  
+}
+
+function newGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement("div")
+            container.appendChild(square)
+            square.style.width = `${size}px`
+            square.style.height = `${size}px`
+            square.style.backgroundColor = "#222"
+            square.style.border = "1px solid black"
+            square.addEventListener("mouseover", (event) => {
+                event.target.style.backgroundColor = "blue"
             })
         }
     }  
