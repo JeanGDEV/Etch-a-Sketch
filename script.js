0a522ba1ddeeff0aee330ed0d2d0f2a18b431867
JeanGDEV <123316415+JeanGDEV@users.noreply.github.com>
2026-08-24 17:22:53 -0400

    Replace many lines of code in `newGrid()` to call `initializeGrid()`

--- script.js
+++ script.js
@@ -30,19 +30,7 @@ function newGrid() {
     while(container.firstChild) {
         container.removeChild(container.firstChild)
     }
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            const square = document.createElement("div")
-            container.appendChild(square)
-            square.style.width = `${size}px`
-            square.style.height = `${size}px`
-            square.style.backgroundColor = "#222"
-            square.style.border = "1px solid black"
-            square.addEventListener("mouseover", (event) => {
-                event.target.style.backgroundColor = "blue"
-            })
-        }
-    }  
+    initializeGrid()
 }
 
 
