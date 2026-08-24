e650d5ca32b63af285b7b24ac1fba1af7f46ab17
JeanGDEV <123316415+JeanGDEV@users.noreply.github.com>
2026-08-24 16:48:31 -0400

    Add `initializeGrid` function

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const container = document.querySelector(".container")
+const resetBtn = document.querySelector(".reset-btn")
+
 
 const containerWidth = 960
+
 const size = containerWidth / 16
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div")
-        container.appendChild(square)
-        square.style.width = `${size}px`
-        square.style.height = `${size}px`
-        square.style.backgroundColor = "#222"
-        square.style.border = "1px solid black"
-        square.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = "tomato"
-        })
-    }
+initializeGrid()
 
+
+function initializeGrid() {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement("div")
+            container.appendChild(square)
+            square.style.width = `${size}px`
+            square.style.height = `${size}px`
+            square.style.backgroundColor = "#222"
+            square.style.border = "1px solid black"
+        }
+    }  
 }
+
+
